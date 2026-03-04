ffa36605630ef247647522d1c9f33628c03bdde6
Victor <victor68346@estudante.ifb.edu.br>
2026-03-04 19:00:46 -0300

    organizacao de dados

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const situacao = document.querySelector("#resultadoConexao")
+const InformarData = document.querySelector("#receberData")
+const btnData = document.querySelector("#btnData")
+
 
 function mostrarSituacao (res) {
    fetch("https://covid19-brazil-api.now.sh/api/status/v1", {
@@ -17,4 +20,33 @@ function mostrarSituacao (res) {
     .catch (err => console.error(err))
 }
 
+;
+
+function casosPorTodoEstado () {
+    fetch("https://covid19-brazil-api.vercel.app/api/report/v1")
+    .then (res => res.json())
+    .then (dados => {
+        const casosPorEstado = dados
+        console.log (casosPorEstado)
+    })
+}
+
+function casosPorData (data) {
+   
+    fetch(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/${data}`) 
+    .then (res => res.json())
+    .then (dados => {
+        const casosPorData = dados.data
+        console.log (casosPorData)
+
+    } )
+}
+
+btnData.addEventListener('click', ()=> {
+    casosPorData(InformarData.value.replace(/-/g,""))
+    console.log (casosPorData)
+    console.log(InformarData.value.replace(/-/g,""));
+ })
+
 mostrarSituacao ();
+casosPorTodoEstado();
