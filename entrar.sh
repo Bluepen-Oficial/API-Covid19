91fb73441b0b2101fff32bbba57729f177afe8a7
Victor <victor68346@estudante.ifb.edu.br>
2026-03-05 19:17:07 -0300

    Organização arquivos

---
+++ entrar.sh
@@ -0,0 +1,4 @@
+git init
+git branch -M main
+git remote add origin https://github.com/Bluepen-Oficial/API-Covid19.git
+git push -u origin main
--- script.js
+++ script.js
@@ -1,53 +1,121 @@
+// Criando variaveis
 const situacao = document.querySelector("#resultadoConexao")
 const InformarData = document.querySelector("#receberData")
 const btnData = document.querySelector("#btnData")
 
+// Criando variaveis do menu superior
+const rankEstados = document.querySelector("#rankEstados");
+const estadosIsolados = document.querySelector("#estadosIsolados");
+const casosAmerica = document.querySelector("#casosAmerica");
+const paisesIsolados = document.querySelector("#paisesIsolados");
 
-function mostrarSituacao (res) {
-   fetch("https://covid19-brazil-api.now.sh/api/status/v1", {
-  "method": "GET"
-})
+// Criando variaveis Rank Estados
+const erroRank = document.querySelector("#erroRank");
+const btnCasos = document.querySelector("#btnCasos");
+const btnMortes = document.querySelector("#btnMortes");
+let siglaEstados = [];
+let armazenarCasos = [];
+let armazenarMortes = [];
 
-    .then (res => res.json())
-    .then (dados => {
-        const validarStatus = dados.status
-        
-        
-        if (validarStatus == "ok" ) {
-            situacao.innerHTML="ONLINE"
-        }
+btnCasos.addEventListener('click', () => { casosPorTodoEstado('Casos') });
+// btnMortes.addEventListener('click', () => { casosPorTodoEstado('Mortes') });
+// btnData.addEventListener('click', () => {
+//     // console.log(InformarData.value.replace(/-/g, ""));
+//     // casosPorData(InformarData.value.replace(/-/g, ""))
+//     limparGrafico(graficoBarras);
+// });
+
+rankEstados.addEventListener('click', () => {
+
+});
+
+function mostrarSituacao() {
+    fetch("https://covid19-brazil-api.now.sh/api/status/v1", {
+        "method": "GET"
     })
-    .catch (err => console.error(err))
+        .then(res => res.json())
+        .then(dados => {
+            const validarStatus = dados.status
+
+            if (validarStatus == "ok") {
+                situacao.innerHTML = "ONLINE";
+                situacao.style.color = "#18eb18";
+            }
+        })
+        .catch(err => console.error(err));
 }
 
-;
-
-function casosPorTodoEstado () {
+function casosPorTodoEstado(busca) {
     fetch("https://covid19-brazil-api.vercel.app/api/report/v1")
-    .then (res => res.json())
-    .then (dados => {
-        const casosPorEstado = dados
-        console.log (casosPorEstado)
-    })
+        .then(res => res.json())
+        .then(dados => {
+            const casosPorEstado = dados;
+
+            console.log(`${casosPorEstado.data[0].uf}: ${casosPorEstado.data[0].cases} casos, ${casosPorEstado.data[0].deaths} mortes.`);
+            console.log(casosPorEstado.data.length)
+            console.log(casosPorEstado);
+
+            for (let i = 0; i < casosPorEstado.data.length; i++) {
+                console.log(`Entrou no for `);
+                siglaEstados.push(casosPorEstado.data[i].uf);
+                armazenarCasos.push(casosPorEstado.data[i].cases);
+                armazenarMortes.push(casosPorEstado.data[i].deaths);
+            }
+
+            console.log(`A > ${busca}, ${siglaEstados}, ${armazenarCasos}}`);
+
+
+            busca == 'Casos' ? exibirGrafico(busca, siglaEstados, armazenarCasos) : exibirGrafico(busca, siglaEstados, armazenarMortes);
+
+        })
+        .catch(() => {
+            erroRank.innerHTML = 'Dados não encontrados!'
+        });
 }
 
-function casosPorData (data) {
-   
-    fetch(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/${data}`) 
-    .then (res => res.json())
-    .then (dados => {
-        const casosPorData = dados.data
-        console.log (casosPorData)
-
-    } )
+function casosPorData(data) {
+    fetch(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/${data}`)
+        .then(res => res.json())
+        .then(dados => {
+            const casosPorData = dados.data
+            console.log(casosPorData)
+        })
 }
 
-btnData.addEventListener('click', ()=> {
-    casosPorData(InformarData.value.replace(/-/g,""))
-    console.log (casosPorData)
-    console.log(InformarData.value.replace(/-/g,""));
- })
 
-mostrarSituacao ();
-casosPorTodoEstado()
-casosPorTodoEstado();
+function exibirGrafico(busca, siglas, dados) {
+    console.log(`B > ${busca}, ${siglas}, ${dados}}`);
+    
+    const graficoEstados = document.querySelector("#myChart");
+    
+    graficoBarras = new Chart(graficoEstados, {
+        type: 'bar',
+        data: {
+            labels: siglas,
+            datasets: [{
+                label: `Quantidade de ${busca}`,
+                data: dados,
+                bordeWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    responsive: true
+                }
+            }
+        }
+    });
+    
+    graficoBarras.update();
+}
+
+function limparGrafico(chart) {
+    chart.data.labels = [];
+    chart.data.datasets.forEach((datasets) => {
+        datasets.data = [];
+    });
+    chart.update();
+}
+
+mostrarSituacao()
